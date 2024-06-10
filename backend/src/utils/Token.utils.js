fc48488682d633b039a1606b7877e9bed1a8d31b
anmol <anmolof10gms@gmail.com>
2024-06-10 10:45:55 +0530

    further development is done

--- backend/src/utils/Token.utils.js
+++ backend/src/utils/Token.utils.js
@@ -1,14 +1,14 @@
-const jwt = require("jsonwebtoken");
+const jwt = require('jsonwebtoken');
 const { PUBLIC_DATA } = require("../../constant")
 
-const generateToken = (user, expire = '1d') {
+const generateToken = (user, expire = '1d') => {
     const token = jwt.sign({userid: user._id}, PUBLIC_DATA.jwt_auth, {expiresIn: expire
     })
     return token;
 }
 
 
-const validateToken = (token) {
+const validateToken = (token) => {
     const token = jwt.verify(token, PUBLIC_DATA.jwt_auth)
     return token;
 }
